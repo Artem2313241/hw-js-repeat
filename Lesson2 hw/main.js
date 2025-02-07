321baed1b93c17e25fcd65e9fa46ae3d0af9e44f
PC <gsttv609@gmail.com>
2025-02-07 18:58:48 +0200

    hw

---
+++ Lesson2 hw/main.js
@@ -0,0 +1,212 @@
+// let array;
+//
+// array = [1,2,3,4,5,6,7,8,`true`,false]
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array[3])
+// console.log(array[4])
+// console.log(array[5])
+// console.log(array[6])
+// console.log(array[7])
+// console.log(array[8])
+// console.log(array[9])
+
+
+// let Season1;
+// let Season2;
+// let Season3;
+//
+// Season1 = {
+//     title:'Supernatural: Season 1',
+//     SeriesCount:22,
+//     genre:'horror',
+//     authors:[{name:'Eric',age:50}]
+// Season2
+// book2 = {
+//     title:'Supernatural: Season 2',
+//     SeriesCount:22,
+//     genre:'horror',
+//     authors:[{name:'Eric',age:50}]
+// }
+// Season3 = {
+//     title:'Supernatural: Season3',
+//     SeriesCount:16,
+//     genre:'horror',
+//     authors:[{name:'Eric',age:50}]
+// }
+
+// let user1 = {
+//     name:'Jared',
+//     username:'Sam',
+//     password:'sad23'
+// }
+// let user2 = {
+//     name:'Jared',
+//     username:'Sam',
+//     password:'sad23'
+// }
+// let user3 = {
+//     name:'Jareeq',
+//     username:'Sam',
+//     password:'sadas23'
+// }
+// let user4 = {
+//     name:'mishja',
+//     username:'adw',
+//     password:'sadczx23'
+// }
+// let user5 = {
+//     name:'Jareadd',
+//     username:'Sawwdm',
+//     password:'saasdd23'
+// }
+// let user6 = {
+//     name:'Jaredzx',
+//     username:'Samcv',
+//     password:'sad23b2'
+// }
+// let user7 = {
+//     name:'Jaredr',
+//     username:'Sazm',
+//     password:'sad23213'
+// }
+// let user8 = {
+//     name:'Jaredzcvb',
+//     username:'Sam2',
+//     password:'sad2343'
+// }
+// // let user9 = {
+// //     name:'Jaredpa',
+// //     username:'Sam5',
+// //     password:'sad2da3'
+// // }
+// // let user10 = {
+// //     name:'Jarecxjkd',
+// //     username:'Swadam',
+// //     password:'sad2asz3'
+// // }
+// //
+// // console.log(user1.password)
+// // console.log(user2.password)
+// // console.log(user3.password)
+// // console.log(user4.password)
+// // console.log(user5.password)
+// // console.log(user6.password)
+// // console.log(user7.password)
+// // console.log(user8.password)
+// // console.log(user9.password)
+// // console.log(user10.password)
+
+// let temps = [
+//     {day:'monday',values:[3,4,3]},
+//     {day:'Tuesday',values:[1,1,1]},
+//     {day:'Wednesday',values:[10,19,8]},
+//     {day:'Thursday',values:[6,13,3]},
+//     {day:'Friday',values:[2,3,-5]},
+//     {day:'Saturday',values:[1,5,0]},
+//     {day:'Sunday',values:[9,15,2]},
+// ]
+//
+// console.log(temps)
+
+// let x = +prompt("enter your number 5 12 0")
+// if (x !== 0){
+//     console.log("right")
+// }else {
+//     console.log('wrong')
+// }
+
+// let time = 33;
+// if (time >= 0 && time < 15){
+//     console.log('1');
+// }else if (time >= 15 && time < 30){
+//     console.log('2')
+// }else if (time >= 30 && time < 45) {
+//     console.log('3')
+// }else if (time >= 45 && time < 60){
+//     console.log('4')
+// }
+
+// let day = 1;
+// if (day >= 1 && day < 10){
+//     console.log('1');
+// }else if (day >= 10 && day < 20){
+//     console.log('2')
+// }else if (day >= 20 && day <31) {
+//     console.log('3')
+
+// switch (1){
+//     case 1:
+//         console.log("breakfast")
+//         console.log('sleep')
+//         console.log("learnJs")
+//         break;
+//     case 2:
+//         console.log("breakfast")
+//         console.log('sleep')
+//         console.log("learnJs")
+//         break;
+//     case 3:
+//         console.log("breakfast")
+//         console.log('sleep')
+//         console.log("learnJs")
+//         break;
+//     case 4:
+//         console.log("breakfast")
+//         console.log('sleep')
+//         console.log("learnJs")
+//         break;
+//     case 5:
+//         console.log("breakfast")
+//         console.log('sleep')
+//         console.log("learnJs")
+//         break;
+//     case 6:
+//         console.log("breakfast")
+//         console.log('sleep')
+//         console.log("learnJs")
+//         break;
+//     case 7:
+//         console.log("breakfast")
+//         console.log('sleep')
+//         console.log("learnJs")
+//         break;
+// }
+
+// let f = 2;
+// let v = 6;
+//
+// if (f > v) {
+//     console.log(f)
+// }else if (v > f){
+//     console.log(v);
+// }else if (f===v){
+//     console.log("===");
+// }
+
+// let x = 0;
+// if (x===0 || x === null || x === undefined || x === NaN || x === ''){
+//     x = 'default';
+// }
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+const course1 = coursesAndDurationArray[0];
+
+if (course1.monthDuration > 5){
+    console.log('super');
+}
+
+const ddsa = coursesAndDurationArray[1];
+if (ddsa.monthDuration > 5) {
+    console.log('super');
+}
+console.log(ddsa)
