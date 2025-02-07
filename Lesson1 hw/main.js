ebe40332c704d0bd99e0e9fdc8e4ecb23e981118
PC <gsttv609@gmail.com>
2025-02-07 18:58:53 +0200

    hw

---
+++ Lesson1 hw/main.js
@@ -0,0 +1,27 @@
+// const a = `hello`;
+// const b = `owu`;
+// const f = `com`;
+// const w =  `ua`;
+// const j = 1;
+// const p = 10;
+// const i = -999;
+// const l = 123;
+// const d = 3.14;
+// const x = 2.7;
+// const v = 16;
+// const o = true;
+// const c = false;
+//
+// console.log(a,b,f,w,j,p,i,l,d,x,v,o,c)
+
+// let firstName = `Заріцький`;
+// let middleName = 'Артем';
+// let lastName =`Сергійовичь`;
+//
+// console.log(firstName+` `+middleName+` `+lastName)
+
+// let a = 100; let b = '100'; let c = true;
+//
+// console.log(typeof a,a,);
+// console.log(typeof b,b);
+// console.log(typeof c,c)
